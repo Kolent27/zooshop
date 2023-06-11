58d4da7c23316c4268ba3e391cfd25a70e9f3a9e
Kolent27 <djabrail.g@outlook.com>
2023-06-12 01:26:35 +0300

    ver 2.2

--- addcart.js
+++ addcart.js
@@ -1,74 +1,77 @@
 const addToCartButtons = document.querySelectorAll('.btn.add-to-cart');
-  const clearCartButton = document.querySelector('.clear-cart');
-  const cartItemsList = document.querySelector('.cart-items');
-  const confirmCartButton = document.querySelector('.confirm-cart');
-  const cartButton = document.getElementById("cart-button");
-  const cart = document.querySelector('.cart');
+const clearCartButton = document.querySelector('.clear-cart');
+const cartItemsList = document.querySelector('.cart-items');
+const confirmCartButton = document.querySelector('.confirm-cart');
+const cartButton = document.getElementById("cart-button");
+const cart = document.querySelector('.cart');
 
-  // Проверяем, есть ли сохраненные данные корзины в localStorage
-  const savedCartItems = localStorage.getItem('myStoreCart');
-  if (savedCartItems) {
-    cartItemsList.innerHTML = savedCartItems;
-  }
+// Проверяем, есть ли сохраненные данные корзины в localStorage
+const savedCartItems = localStorage.getItem('myStoreCart');
+if (savedCartItems) {
+  cartItemsList.innerHTML = savedCartItems;
+}
 
-  addToCartButtons.forEach(button => {
-    button.addEventListener('click', addToCart);
-  });
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
 
-  clearCartButton.addEventListener('click', clearCart);
+clearCartButton.addEventListener('click', clearCart);
 
-  confirmCartButton.addEventListener('click', redirectToCheckout);
+confirmCartButton.addEventListener('click', redirectToCheckout);
 
-  cartButton.addEventListener("click", function() {
-    cart.classList.toggle('show');
-  });
+cartButton.addEventListener("click", function () {
+  cart.classList.toggle('show');
+});
 
-  function addToCart(event) {
-    const product = event.target.closest('.product');
+function addToCart(event) {
+  event.preventDefault(); // Предотвращаем отправку формы по умолчанию
 
-    const productImgSrc = product.querySelector('img').src;
-    const productTitle = product.querySelector('h3 a').textContent;
-    const productPrice = product.querySelector('.price').textContent;
+  const product = event.target.closest('.product');
+  const productImgSrc = product.querySelector('img').src;
+  const productTitle = product.querySelector('h3 a').textContent;
+  const productPrice = product.querySelector('.price').textContent;
 
-    const cartItem = document.createElement('li');
-    cartItem.innerHTML = `
-      <img src="${productImgSrc}" alt="Фото">
-      <h4>${productTitle}</h4>
-      <span>${productPrice}</span>
-    `;
+  const cartItem = document.createElement('li');
+  cartItem.innerHTML = `
+    <img src="${productImgSrc}" alt="Фото">
+    <h4>${productTitle}</h4>
+    <span>${productPrice}</span>
+  `;
 
-    cartItemsList.appendChild(cartItem);
+  cartItemsList.appendChild(cartItem);
 
-    // сохраняем корзину в localStorage
-    const cartItems = cartItemsList.innerHTML;
-    localStorage.setItem('myStoreCart', cartItems);
-  }
+  // сохраняем корзину в localStorage
+  const cartItems = cartItemsList.innerHTML;
+  localStorage.setItem('myStoreCart', cartItems);
+}
 
-  function clearCart() {
-    // очищаем список корзины
-    cartItemsList.innerHTML = '';
+function clearCart() {
+  // очищаем список корзины
+  cartItemsList.innerHTML = '';
 
-    // удаляем сохраненные данные корзины из localStorage
-    localStorage.removeItem('myStoreCart');
-  }
+  // удаляем сохраненные данные корзины из localStorage
+  localStorage.removeItem('myStoreCart');
+}
 
-  function redirectToCheckout() {
-    // Получаем список товаров в корзине
-    const cartItems = cartItemsList.innerHTML;
+function redirectToCheckout(event) {
+  event.preventDefault(); // Предотвращаем отправку формы по умолчанию
 
-    // Создаем форму для отправки списка товаров на другой сайт
-    const form = document.createElement('form');
-    form.setAttribute('method', 'get');
-    form.setAttribute('action', 'cart.html');
+  // Получаем список товаров в корзине
+  const cartItems = cartItemsList.innerHTML;
 
-    // Создаем скрытое поле формы для отправки списка товаров
-    const input = document.createElement('input');
-    input.setAttribute('type', 'hidden');
-    input.setAttribute('name', 'cartItems');
-    input.setAttribute('value', cartItems);
-    form.appendChild(input);
+  // Создаем форму для отправки списка товаров на другой сайт
+  const form = document.createElement('form');
+  form.setAttribute('method', 'GET');
+  form.setAttribute('action', 'cart.html');
 
-    // Добавляем форму на страницу и отправляем данные на другой сайт
-    document.body.appendChild(form);
-    form.submit();
-  }
+  // Создаем скрытое поле формы для отправки списка товаров
+  const input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'cartItems');
+  input.setAttribute('value', cartItems);
+  form.appendChild(input);
+
+  // Добавляем форму на страницу и отправляем данные на другой сайт
+  document.body.appendChild(form);
+  form.submit();
+}
